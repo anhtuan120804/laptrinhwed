199a1bb1b37658f7e24c4268ad7475b7064a4f01
anhtuan120804 <anhtuan12082004@gmail.com>
2025-04-04 12:01:38 +0700

    create file

--- example-app/app/Http/Controllers/CrudUserController.php
+++ example-app/app/Http/Controllers/CrudUserController.php
@@ -2,9 +2,160 @@
 
 namespace App\Http\Controllers;
 
+use Hash;
+use Session;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
+/**
+ * CRUD User controller
+ */
 class CrudUserController extends Controller
 {
-    //
+
+    /**
+     * Login page
+     */
+    public function login()
+    {
+        return view('crud_user.loginnew');
+    }
+
+    /**
+     * User submit form login
+     */
+    public function authUser(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'password' => 'required',
+        ]);
+
+        $credentials = $request->only('name', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('list')
+                ->withSuccess('Signed in');
+        }
+
+        return redirect("login")->withSuccess('Login details are not valid');
+    }
+
+    /**
+     * Registration page
+     */
+    public function createUser()
+    {
+        return view('crud_user.register');
+    }
+
+    /**
+     * User submit form register
+     */
+    public function postUser(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+
+        $data = $request->all();
+        $check = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'massv' => $data['massv'],
+            'address' => $data['address'],
+            'password' => Hash::make($data['password'])
+        ]);
+
+        return redirect("login");
+    }
+
+    /**
+     * View user detail page
+     */
+    public function readUser(Request $request)
+    {
+        $user_id = $request->get('id');
+        $user = User::find($user_id);
+
+        return view('crud_user.view', ['messi' => $user]);
+    }
+
+    /**
+     * Delete user by id
+     */
+    public function deleteUser(Request $request)
+    {
+        $user_id = $request->get('id');
+        $user = User::destroy($user_id);
+
+        return redirect("list")->withSuccess('You have signed-in');
+    }
+
+    /**
+     * Form update user page
+     */
+    public function updateUser(Request $request)
+    {
+        $user_id = $request->get('id');
+        $user = User::find($user_id);
+
+        return view('crud_user.updatenew', ['user' => $user]);
+    }
+
+    /**
+     * Submit form update user
+     */
+    public function postUpdateUser(Request $request)
+    {
+        $input = $request->all();
+
+        if (!isset($input['id'])) {
+            return redirect()->back()->withErrors(['error' => 'Thiếu ID người dùng!']);
+        }
+
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,id,'.$input['id'],
+            'password' => 'required|min:6',
+        ]);
+
+       $user = User::find($input['id']);
+       $user->name = $input['name'];
+       $user->email = $input['email'];
+       $user->massv = $input['massv'];
+       $user->address = $input['address'];
+       $user->password = $input['password'];
+       $user->save();
+
+        return redirect("list")->withSuccess('You have signed-in');
+    }
+
+
+    /**
+     * List of users
+     */
+    public function listUser()
+    {
+        if (Auth::check()) {
+            $users = User::all();
+            return view('crud_user.listnew', ['users' => $users]);
+        }
+
+        return redirect("login")->withSuccess('You are not allowed to access');
+    }
+
+    /**
+     * Sign out
+     */
+    public function signOut()
+    {
+        Session::flush();
+        Auth::logout();
+
+        return Redirect('login');
+    }
 }
---
+++ example-app/resources/views/crud_user/updatenew.blade.php
@@ -0,0 +1,112 @@
+@section('content')
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cập nhật thông tin</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <!-- Navigation -->
+        <div class="row border p-2 text-center mb-4">
+            <div class="col">
+                @guest
+                <a href="{{route('login')}}" class="text-decoration-none">Home</a> /
+                <a href="{{route('login')}}" class="text-decoration-none">Đăng nhập</a>/
+                <a href="{{ route('user.createUser') }}" class="text-decoration-none">Đăng ký</a>
+                @else
+                <a href="{{route('login')}}" class="text-decoration-none">Home</a> /
+                <a href="{{ route('user.list') }}" class="text-decoration-none">Users</a> /
+                <a href="{{ route('signout') }}" class="text-decoration-none">Đăng xuất</a>
+                @endguest
+            </div>
+        </div>
+
+        <!-- Update Profile Form -->
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title text-center mb-4">Màn hình cập nhật</h4>
+                        <form action="{{ route('user.postUpdateUser') }}" method="POST">
+                            @csrf
+                            <input name="id" type="hidden" value="{{$user->id}}">
+                            <div class="mb-3">
+                                <label for="username" class="form-label">Username</label>
+                                <input type="text" placeholder="Name" id="name" class="form-control" name="name"
+                                    value="{{ $user->name }}"
+                                    required autofocus>
+                                @if ($errors->has('name'))
+                                <span class="text-danger">{{ $errors->first('name') }}</span>
+                                @endif
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Mật khẩu</label>
+                                <input type="password" placeholder="Password" id="password" class="form-control"
+                                    name="password" required>
+                                @if ($errors->has('password'))
+                                <span class="text-danger">{{ $errors->first('password') }}</span>
+                                @endif
+                            </div>
+                            <div class="mb-3">
+                                <div class="mb-3">
+                                    <label for="password_confirmation" class="form-label">Nhập lại mật khẩu</label>
+                                    <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email</label>
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="Email" id="email_address" class="form-control"
+                                        value="{{ $user->email }}"
+                                        name="email" required autofocus>
+                                    @if ($errors->has('email'))
+                                    <span class="text-danger">{{ $errors->first('email') }}</span>
+                                    @endif
+                                </div>
+                                <label for="massv" class="form-label">massv</label>
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="massv" id="massv" class="form-control"
+                                        value="{{ $user->massv }}"
+                                        name="massv" required autofocus>
+                                    @if ($errors->has('massv'))
+                                    <span class="text-danger">{{ $errors->first('massv') }}</span>
+                                    @endif
+                                </div>
+                                <label for="address" class="form-label">address</label>
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="address" id="address" class="form-control"
+                                        value="{{ $user->address }}"
+                                        name="address" required autofocus>
+                                    @if ($errors->has('address'))
+                                    <span class="text-danger">{{ $errors->first('address') }}</span>
+                                    @endif
+                                </div>
+                                <div class="d-flex justify-content-between align-items-center">
+                                    <a href="#" class="text-decoration-none">Đã có tài khoản</a>
+                                    <button type="submit" class="btn btn-dark btn-block">Update</button>
+                                </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Footer -->
+        <div class="row border p-2 text-center mt-4">
+            <div class="col">
+                Lập trình web @01024
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
