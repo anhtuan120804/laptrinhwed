94c4c9e080ba6609a0b09634cce9e26e421694be
anhtuan120804 <anhtuan12082004@gmail.com>
2025-04-04 15:51:32 +0700

    create

--- example-app/database/migrations/2014_10_12_000000_create_users_table.php
+++ example-app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-
+            $table->string('agr');
+            $table->url('fb');
+            
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
@@ -23,7 +25,7 @@ return new class extends Migration
         });
     }
 
-    
+
     /**
      * Reverse the migrations.
      */
